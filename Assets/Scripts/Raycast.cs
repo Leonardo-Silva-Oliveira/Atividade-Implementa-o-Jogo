5c1483f1a7fc96c0929a559efe4e5a95ac45e467
Leonardo-Silva-Oliveira <leonardo.oliveira611@etec.sp.gov.br>
2022-05-19 21:17:39 -0300

    Ajustes na cena de raycast

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -10,22 +10,38 @@ public class Raycast : MonoBehaviour
     float velocidadeprojetil;
     [SerializeField]
     Transform posicaoprojetil;
-    [SerializeField]
-    Transform castPoint;
+
+    bool playercheck;
+    float intervalotiro;
     void Start(){
+        playercheck = false;
+        intervalotiro = 1;
     }
 
     // Update is called once per frame
     void Update(){
 
 
-        RaycastHit2D hit = Physics2D.Raycast(castPoint.position, Vector2.left);
+
+        RaycastHit2D hit = Physics2D.Raycast(posicaoprojetil.position, Vector2.left, 10.0f);
         if(hit.collider != null){
             Debug.Log("Acertou");
+            playercheck = true;
+            intervalotiro = intervalotiro + Time.deltaTime;
+        }
+        else
+        {
+            playercheck = false;
+        }
+        
+        if(playercheck && intervalotiro >= 1){
+            Atirar();
+            intervalotiro = 0;
         }
 
     }
     void Atirar(){
+
         Rigidbody2D prefab = Instantiate(projetil, posicaoprojetil.transform.position, posicaoprojetil.transform.localRotation);
         prefab.velocity = -transform.right * (velocidadeprojetil * Time.deltaTime * 2);
     }
